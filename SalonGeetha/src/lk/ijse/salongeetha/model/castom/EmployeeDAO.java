cf18f9fe8d60e5f71562f254d5cef0784fc75f2a
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-01-12 09:03:44 -0800

    Refactor EmployeeModel

--- SalonGeetha/src/lk/ijse/salongeetha/model/castom/EmployeeDAO.java
+++ SalonGeetha/src/lk/ijse/salongeetha/model/castom/EmployeeDAO.java
@@ -1,4 +1,28 @@
 package lk.ijse.salongeetha.model.castom;
 
-public interface EmployeeDAO {
+import lk.ijse.salongeetha.db.DBConnection;
+import lk.ijse.salongeetha.model.CrudUtil;
+import lk.ijse.salongeetha.model.SQLUtil;
+import lk.ijse.salongeetha.model.castom.impl.UserModel;
+import lk.ijse.salongeetha.to.Employee;
+import lk.ijse.salongeetha.to.User;
+import org.hibernate.SQLQuery;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface EmployeeDAO extends SQLUtil<Employee> {
+    ArrayList<Employee> getBeauticians() throws SQLException, ClassNotFoundException;
+
+    ArrayList<Employee> searchEmployeeDetails(Employee employee) throws SQLException, ClassNotFoundException;
+
+
+    boolean addReceptionist(Employee employee, User user) throws SQLException, ClassNotFoundException;
+
+    Employee getEmployeeJobTitle(User user) throws SQLException, ClassNotFoundException;
+
+    boolean checkAdmin(Employee employee) throws SQLException, ClassNotFoundException;
+
+    boolean updateAdmin(Employee employee) throws SQLException, ClassNotFoundException;
 }
--- SalonGeetha/src/lk/ijse/salongeetha/model/castom/impl/EmployeeModel.java
+++ SalonGeetha/src/lk/ijse/salongeetha/model/castom/impl/EmployeeModel.java
@@ -2,6 +2,8 @@ package lk.ijse.salongeetha.model.castom.impl;
 
 import lk.ijse.salongeetha.db.DBConnection;
 import lk.ijse.salongeetha.model.CrudUtil;
+import lk.ijse.salongeetha.model.castom.CustomerDAO;
+import lk.ijse.salongeetha.model.castom.EmployeeDAO;
 import lk.ijse.salongeetha.model.castom.UserDAO;
 import lk.ijse.salongeetha.to.Employee;
 import lk.ijse.salongeetha.to.User;
@@ -12,46 +14,23 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class EmployeeModel {
-
-    public static boolean addReceptionist(Employee employee, User user) throws SQLException, ClassNotFoundException {
-        try {
-            DBConnection.getDBConnection().getConnection().setAutoCommit(false);
-
-            boolean isAdded = CrudUtil.setQuery("INSERT INTO Employee VALUES (?,?,?,?,?,?,?,?,?)", employee.getEmpId(), employee.getName()
-                    , employee.getAddress(), employee.getDob(), employee.getPhoneNumber(), employee.getDescription(), employee.getEmail()
-                    , employee.getNic(), employee.getJobTitle());
-            if (isAdded) {
-                UserDAO userDAO=new UserModel();
-                boolean addUser = userDAO.add(user);
-                if (addUser) {
-                    DBConnection.getDBConnection().getConnection().commit();
-                    return true;
-                }
-            }
-            DBConnection.getDBConnection().getConnection().rollback();
-            return false;
-        } finally {
-            DBConnection.getDBConnection().getConnection().setAutoCommit(true);
-        }
-    }
-
-    public static boolean addEmployee(Employee employee) throws SQLException, ClassNotFoundException {
+public class EmployeeModel implements EmployeeDAO {
+    public boolean add(Employee employee) throws SQLException, ClassNotFoundException {
         return CrudUtil.setQuery("INSERT INTO Employee VALUES (?,?,?,?,?,?,?,?,?)", employee.getEmpId(), employee.getName(),
                 employee.getAddress(), employee.getDob(), employee.getPhoneNumber(), employee.getDescription(), employee.getEmail(),
                 employee.getNic(), employee.getJobTitle());
     }
 
-    public static boolean deleteEmployee(Employee employee) throws SQLException, ClassNotFoundException {
+    public boolean delete(Employee employee) throws SQLException, ClassNotFoundException {
         return CrudUtil.setQuery("DELETE FROM Employee WHERE Emp_Id=?", employee.getEmpId());
     }
 
-    public static boolean updateEmployee(Employee employee) throws SQLException, ClassNotFoundException {
+    public boolean update(Employee employee) throws SQLException, ClassNotFoundException {
         return CrudUtil.setQuery("UPDATE Employee SET Name=?,Address=?,Phone_number=?,Description=?,Email=? WHERE Emp_Id=?"
                 , employee.getName(), employee.getAddress(), employee.getPhoneNumber(), employee.getDescription(), employee.getEmail(), employee.getEmpId());
     }
 
-    public static String checkId() throws SQLException, ClassNotFoundException {
+    public String checkId() throws SQLException, ClassNotFoundException {
         ResultSet resultSet = CrudUtil.setQuery("SELECT Emp_Id FROM employee ORDER BY Emp_Id DESC LIMIT 1");
         if (resultSet.next()) {
             return String.valueOf(resultSet.getObject(1));
@@ -59,32 +38,7 @@ public class EmployeeModel {
         return null;
     }
 
-    public static boolean checkAdmin(Employee employee) throws SQLException, ClassNotFoundException {
-        ResultSet resultSet = CrudUtil.setQuery("SELECT Phone_number FROM Employee WHERE Job_title='Admin'");
-        if (resultSet.next()) {
-            employee.setPhoneNumber(String.valueOf(resultSet.getObject(1)));
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean updateAdmin(Employee employee) throws SQLException, ClassNotFoundException {
-        return CrudUtil.setQuery("UPDATE Employee SET Address=?,DOB=?,Phone_number=?,Description=? WHERE Job_title='Admin'"
-                , employee.getAddress(), employee.getDob(), employee.getPhoneNumber(), employee.getDescription());
-    }
-
-    public static Employee getEmployeeJobTitle(User user) throws SQLException, ClassNotFoundException {
-        ResultSet resultSet = CrudUtil.setQuery("SELECT e.Job_title FROM Employee e JOIN User u ON  e.Emp_Id = u.Emp_Id WHERE u.User_name=?"
-                , user.getUserName());
-        if (resultSet.next()) {
-            Employee employee = new Employee();
-            employee.setJobTitle(String.valueOf(resultSet.getObject(1)));
-            return employee;
-        }
-        return null;
-    }
-
-    public static ArrayList<Employee> getAllEmployee() throws SQLException, ClassNotFoundException {
+    public ArrayList<Employee> getAll() throws SQLException, ClassNotFoundException {
         ArrayList<Employee> employees = new ArrayList<>();
         ResultSet resultSet = CrudUtil.setQuery("SELECT * FROM Employee");
         if (resultSet.next()) {
@@ -106,7 +60,7 @@ public class EmployeeModel {
         return new ArrayList<>();
     }
 
-    public static ArrayList<Employee> searchEmployee(Employee employee) throws SQLException, ClassNotFoundException {
+    public ArrayList<Employee> search(Employee employee) throws SQLException, ClassNotFoundException {
         ArrayList<Employee> employees = new ArrayList<>();
         String setColumn;
         Pattern namePattern = Pattern.compile("[a-zA-Z]{1,}");
@@ -134,7 +88,8 @@ public class EmployeeModel {
         return employees;
     }
 
-    public static ArrayList<Employee> getBeauticians() throws SQLException, ClassNotFoundException {
+
+    public ArrayList<Employee> getBeauticians() throws SQLException, ClassNotFoundException {
         ResultSet resultSet = CrudUtil.setQuery("SELECT Emp_Id FROM Employee WHERE Job_title='Beautician'");
         ArrayList<Employee> employees = new ArrayList<>();
         while (resultSet.next()) {
@@ -146,7 +101,7 @@ public class EmployeeModel {
 
     }
 
-    public static ArrayList<Employee> searchEmployeeDetails(Employee employee) throws SQLException, ClassNotFoundException {
+    public ArrayList<Employee> searchEmployeeDetails(Employee employee) throws SQLException, ClassNotFoundException {
         ArrayList<Employee> employees = new ArrayList<>();
         ResultSet resultSet = CrudUtil.setQuery("SELECT * FROM Employee WHERE Emp_Id = ?", employee.getEmpId());
         while (resultSet.next()) {
@@ -163,5 +118,53 @@ public class EmployeeModel {
             employees.add(searchEmployee);
         }
         return employees;
+    }
+
+
+    public boolean addReceptionist(Employee employee, User user) throws SQLException, ClassNotFoundException {
+        try {
+            DBConnection.getDBConnection().getConnection().setAutoCommit(false);
+
+            boolean isAdded = CrudUtil.setQuery("INSERT INTO Employee VALUES (?,?,?,?,?,?,?,?,?)", employee.getEmpId(), employee.getName()
+                    , employee.getAddress(), employee.getDob(), employee.getPhoneNumber(), employee.getDescription(), employee.getEmail()
+                    , employee.getNic(), employee.getJobTitle());
+            if (isAdded) {
+                UserDAO userDAO=new UserModel();
+                boolean addUser = userDAO.add(user);
+                if (addUser) {
+                    DBConnection.getDBConnection().getConnection().commit();
+                    return true;
+                }
+            }
+            DBConnection.getDBConnection().getConnection().rollback();
+            return false;
+        } finally {
+            DBConnection.getDBConnection().getConnection().setAutoCommit(true);
+        }
+    }
+
+    public Employee getEmployeeJobTitle(User user) throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.setQuery("SELECT e.Job_title FROM Employee e JOIN User u ON  e.Emp_Id = u.Emp_Id WHERE u.User_name=?"
+                , user.getUserName());
+        if (resultSet.next()) {
+            Employee employee = new Employee();
+            employee.setJobTitle(String.valueOf(resultSet.getObject(1)));
+            return employee;
+        }
+        return null;
+    }
+
+    public boolean checkAdmin(Employee employee) throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.setQuery("SELECT Phone_number FROM Employee WHERE Job_title='Admin'");
+        if (resultSet.next()) {
+            employee.setPhoneNumber(String.valueOf(resultSet.getObject(1)));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateAdmin(Employee employee) throws SQLException, ClassNotFoundException {
+        return CrudUtil.setQuery("UPDATE Employee SET Address=?,DOB=?,Phone_number=?,Description=? WHERE Job_title='Admin'"
+                , employee.getAddress(), employee.getDob(), employee.getPhoneNumber(), employee.getDescription());
     }
 }
